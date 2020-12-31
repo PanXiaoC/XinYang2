abee9a7a05cf693d2e274b4b62831c7b02bd5ac5
PanXiaoC <2770421762@qq.com>
2020-12-31 15:00:47 +0800

    相关配置

---
+++ src/main/java/com/example/demo/Pojo/user.java
@@ -0,0 +1,21 @@
+package com.example.demo.Pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class user implements Serializable {
+    private static final long SerVersionUID = 1L;
+    private  Integer userid;
+    private String username;
+    private String userpass;
+    private  String sex;
+}
