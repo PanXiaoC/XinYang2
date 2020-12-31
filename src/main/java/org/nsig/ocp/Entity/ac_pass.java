40300514750b4f0a902a21b7d033f1160aec2dfd
TMDK-LYY <l2385400407@gmail.com>
2020-12-31 16:29:44 +0800

    test

--- src/main/java/org/nsig/ocp/Entity/ac_pass.java
+++ src/main/java/org/nsig/ocp/Entity/ac_pass.java
@@ -3,22 +3,20 @@ package org.nsig.ocp.Entity;
 public class ac_pass {
     private String pass_usern;
     private String pass_pass;
-    public String Getpass_usern() {
 
-        return pass_usern;
-    }
-
-    public void Setpass_usern(String pass_usern) {
+    public ac_pass(String pass_usern) {
         this.pass_usern = pass_usern;
     }
 
-    public String Getpass_pass() {
+    public void setPass_pass(String pass_pass) {
+        this.pass_pass = pass_pass;
+    }
 
+    public String getPass_pass() {
         return pass_pass;
     }
 
-    public void Setpass_pass(String pass_pass) {
-        this.pass_usern = pass_pass;
+    public void setPass_usern(String pass_usern) {
+        this.pass_usern = pass_usern;
     }
-
 }
