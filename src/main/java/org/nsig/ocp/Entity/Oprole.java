69eac6a5a6f9fde484f1ba15ade7e7094d88cf96
908725392 <908725392@qq.com>
2021-01-06 19:30:53 +0800

    启用禁用

--- src/main/java/org/nsig/ocp/Entity/Oprole.java
+++ src/main/java/org/nsig/ocp/Entity/Oprole.java
@@ -14,6 +14,38 @@ import java.io.Serializable;
 @Component
 @ConfigurationProperties(prefix = "oprole")
 public class Oprole implements Serializable {
+    public Integer getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(Integer role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public String getRole_des() {
+        return role_des;
+    }
+
+    public void setRole_des(String role_des) {
+        this.role_des = role_des;
+    }
+
+    public String getRole_con() {
+        return role_con;
+    }
+
+    public void setRole_con(String role_con) {
+        this.role_con = role_con;
+    }
+
     private Integer role_id;
     private String role_name;
     private String role_des;
